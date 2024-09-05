aa38f24ec231c0a1e058159d4f96594d30e5673e
Harminder Puri <harminderpuri18@gmail.com>
2024-09-05 00:25:58 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 import dash
 from dash import html, dcc
 from dash.dependencies import Input, Output
@@ -13,15 +14,22 @@ from app.dashboard import (
     generate_optimization_suggestions
 )
 
+# Get the directory of the current file
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the path to the data file
+data_file_path = os.path.join(current_dir, '..', 'data', 'ad_data.csv')
+
 # Load your data here
-df = pd.read_csv('../data/ad_data.csv')
+df = pd.read_csv(data_file_path)
 df['date'] = pd.to_datetime(df['date'])
 
 app = dash.Dash(__name__)
-server = app.server
+server = app.server  # This is the line needed for Gunicorn
 
 app.layout = create_layout(df)
 
+# Your callback functions here
 @app.callback(
     Output('revenue-graph', 'figure'),
     Input('date-range', 'start_date'),
